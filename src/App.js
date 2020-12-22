5dc4e79bd11a869e4ee44aa94ef3df3936f61172
pangzixiang <313227220@qq.com>
2020-12-22 22:24:02 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>hello react-electron-template !</h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default App;
